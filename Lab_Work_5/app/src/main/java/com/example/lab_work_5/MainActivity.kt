bb62f8fea158c9aef2c23e3123c1d6430d7ad80e
BohdanLytvynov <blitvinov19@gmail.com>
2025-05-12 00:15:48 +0300

    Lab 7 done

--- Lab_Work_5/app/src/main/java/com/example/lab_work_5/MainActivity.kt
+++ Lab_Work_5/app/src/main/java/com/example/lab_work_5/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        this.setTheme(R.style.ThemeOverlay_AppTheme_HighContrast)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
